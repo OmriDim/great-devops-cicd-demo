1ab89af7fd11e39b09d1d5dfdcae020ad9f8c0ee
Omri Dim <omridim.1@gmail.com>
2025-09-01 11:01:14 +0300

    added script and initial CICD

---
+++ mentee_robot.py
@@ -0,0 +1,8 @@
+from time import sleep
+
+while True:
+    print("System check complete. Mentee robot is operational...")
+    sleep(5)
+    print("Ugh, so tired. Entering standby mode. Wake me up later.")
+    sleep(10)
+    print("\n" + "-" * 30 + "\n")  # Add a gap in the terminal between each run
